0da781b3c96acd022e7f1980bf27a10ecf977c86
DNSLV-PMTKV <pometkov.d@protonmail.com>
2020-10-24 17:42:21 +0300

    tried to add mail sending

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -135,3 +135,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = 'users.User'
+
+# SMTP Mail service with decouple
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.conf import settings
 from rest_framework.generics import CreateAPIView
 
 from .serializers import RegisterSerializer
+from .models import UnconfirmedUser, User
+
+
+def send_confirmation_email(to_mail: str, token: str):
+
+    subject = 'Alo?'
+    message = 'THIS IS YOUR TOKEN: {}'.format(token)
+    from_mail = settings.EMAIL_HOST_USER
+
+    send_mail(subject, message, from_mail, [to_mail])
 
 
 class RegisterView(CreateAPIView):
@@ -9,3 +21,11 @@ class RegisterView(CreateAPIView):
 
     serializer_class = RegisterSerializer
     queryset = get_user_model().objects.all()
+
+    # def create(self, request, *args, **kwargs):
+    #     response = super().create(request, *args, **kwargs)
+    #     unconfirmed_user = UnconfirmedUser.objects.get(
+    #         user=response.data.get('id'))
+    #     send_confirmation_email(response.data.get(
+    #         'email'), unconfirmed_user.token)
+    #     return response
