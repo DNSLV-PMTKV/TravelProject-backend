579337a1a01746aaa779ddfd7b2338834648f3c4
DNSLV-PMTKV <pometkov.d@protonmail.com>
2020-10-30 12:38:50 +0200

    Added swagger docs and readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Travel Project
+
+### How to run locally:
+
+1. Create a Python 3.8.5 virtualenv
+
+2. Install dependencies:
+    ```
+    pip install -r requirements/dev.txt
+    ```
+
+3. Create database
+   ```
+    createuser -d travelproject --superuser
+    createdb -O travelproject travelproject
+   ```
+
+4. Create tables
+    ```
+    python manage.py migrate
+    ```
+
+5. Run the server
+    ```
+    python manage.py runserver
+    ```
--- travelproject/urls.py
+++ travelproject/urls.py
@@ -16,11 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+SchemaView = get_schema_view(
+    openapi.Info(title="Snippets API", default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 api_urls = [
     path('', include('users.urls'))
 ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(api_urls))
+    path('api/', include(api_urls)),
+    path("swagger-docs/", SchemaView.with_ui(
+        "swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
