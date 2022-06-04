a9fade3dc33b9855d63c5d7d96c3a2b832cc5869
DNSLV-PMTKV <pometkov.d@protonmail.com>
2022-06-04 20:02:53 +0300

    static files

--- config/urls.py
+++ config/urls.py
@@ -26,7 +26,7 @@ api_urls = [path("users/", include("travelproject.users.urls"))]
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(api_urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
